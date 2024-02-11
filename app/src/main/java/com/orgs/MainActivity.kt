59ab89cbb50428f59b3e54ae3309f3d5781419b7
Vinicius Gomes Correia <viniciusgomesccc10@gmail.com>
2024-02-11 09:58:30 -0300

    criando o arquivo de layout da activity

--- app/src/main/java/com/orgs/MainActivity.kt
+++ app/src/main/java/com/orgs/MainActivity.kt
@@ -2,13 +2,14 @@ package com.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Hello, World", Toast.LENGTH_SHORT).show()
+        setContentView(R.layout.activity_main)
     }
 
 }
